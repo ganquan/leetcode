691e027f9e73d42b925e00fd29eec9d7a7c50775
Gan Quan <imganquan@gmail.com>
2022-03-24 11:36:07 +0800

    Update TwoSum.swift
    using map solution
    much better performance

--- code/swift/TwoSum/TwoSum.swift
+++ code/swift/TwoSum/TwoSum.swift
@@ -1,4 +1,5 @@
 class Solution {
+    //暴利查找
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
          var result: [Int]?
         
@@ -16,5 +17,50 @@ class Solution {
         }
         
         return result ?? [0]
+    }
+
+    //using map
+    func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+        var result: [Int] = []
+
+        //turn array to map
+        //key: element of the array
+        //value: index of the element in the array
+        var tmpMap: [Int: [Int]] = [:]
+
+        //init the map
+        for i in 0...nums.count - 1 {
+            
+            if let _ = tmpMap[nums[i]] {
+                tmpMap[nums[i]]?.append(i)
+            } else {
+                tmpMap[nums[i]] = [i]
+            }
+        }
+        
+        //search
+        for i in 0...nums.count - 1 {
+
+            let distance = target - nums[i]
+
+            if let arr = tmpMap[distance] {
+                if arr.count > 1 {
+                    let j = arr.first(where: {$0 != i})!
+                    result = [i, j]
+                    break
+                } else if arr.first != i {
+                    result = [i, arr.first!]
+                    break
+                } else {
+                    continue
+                }
+            } else {
+                continue
+            }
+
+        }
+
+        return result
+    
     }
 }
